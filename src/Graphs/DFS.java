9925b95a1fa0a4caabe1123c0a51863025f90316
Ramya9M <mramya.tvm00@gmail.com>
2024-04-18 19:28:47 +0530

    DFS implementation

---
+++ src/Graphs/DFS.java
@@ -0,0 +1,25 @@
+package Graphs;
+
+import java.util.*;
+public class DFS {
+
+
+    public void DFSHelper(int node,GraphUsingAdjList graphUsingAdjList, boolean[] visited){
+
+        System.out.println(" "+node+" ");
+        visited[node] = true;
+
+        Iterator<Integer> it = graphUsingAdjList.getAdjList()[node].listIterator();
+        while(it.hasNext()){
+
+            int nbr = it.next();
+            if(!visited[nbr])
+                DFSHelper(nbr,graphUsingAdjList,visited);
+        }
+
+    }
+    public void DFSSearch(int src, int V, GraphUsingAdjList graphUsingAdjList){
+        boolean visited[] = new boolean[V];
+        DFSHelper(src,graphUsingAdjList, visited);
+    }
+}
--- src/Graphs/GraphMain.java
+++ src/Graphs/GraphMain.java
@@ -4,32 +4,36 @@ public class GraphMain {
 
     public static void main(String args[]) {
 
-        GraphUsingEdgeList g1 = new GraphUsingEdgeList(4);
-        g1.addEdge(0, 1, 10);
-        g1.addEdge(0, 2, 20);
-        g1.addEdge(1, 2, 30);
-        g1.addEdge(2, 0, 40);
-        g1.addEdge(2, 3, 50);
-        g1.addEdge(3, 3, 60);
-        System.out.println(g1);
+//        GraphUsingEdgeList g1 = new GraphUsingEdgeList(4);
+//        g1.addEdge(0, 1, 10);
+//        g1.addEdge(0, 2, 20);
+//        g1.addEdge(1, 2, 30);
+//        g1.addEdge(2, 0, 40);
+//        g1.addEdge(2, 3, 50);
+//        g1.addEdge(3, 3, 60);
+//        System.out.println(g1);
+//
+//        GraphUsingAdjMatrix g2 = new GraphUsingAdjMatrix(4);
+//        g2.addEdge(0, 1);
+//        g2.addEdge(0, 2);
+//        g2.addEdge(1, 2);
+//        g2.addEdge(2, 0);
+//        g2.addEdge(2, 3);
+//        g2.addEdge(3, 3);
+//        System.out.println(g2);
 
-        GraphUsingAdjMatrix g2 = new GraphUsingAdjMatrix(4);
-        g2.addEdge(0, 1);
-        g2.addEdge(0, 2);
-        g2.addEdge(1, 2);
-        g2.addEdge(2, 0);
-        g2.addEdge(2, 3);
-        g2.addEdge(3, 3);
-        System.out.println(g2);
-
-        GraphUsingAdjList g3 = new GraphUsingAdjList(4);
+        GraphUsingAdjList g3 = new GraphUsingAdjList(6);
+        g3.addEdge(0,1,true);
         g3.addEdge(1,2,true);
-        g3.addEdge(0,3,true);
-        g3.addEdge(1,3,true);
         g3.addEdge(2,3,true);
+        g3.addEdge(3,4,true);
+        g3.addEdge(1,5,true);
         System.out.println(g3);
 
-        BFS bfs = new BFS();
-        bfs.BFSSearch(4,0,g3);
+//        BFS bfs = new BFS();
+//        bfs.BFSSearch(4,0,g3);
+
+        DFS dfs = new DFS();
+        dfs.DFSSearch(0,6,g3);
     }
 }
